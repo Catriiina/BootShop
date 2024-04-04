4429a64ba9f05c620473aceec8ff6744def864f7
Catriiina <catherinaignatovich@gmail.com>
2024-04-04 10:40:23 +0200

    add adidas models

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,59 @@
 import React, {useState} from 'react';
 import styles from "./components/Site.module.css";
-import {PageOne} from "./components/pages/PageOne";
-import {PageTwo} from "./components/pages/PageTwo";
-import {PageThree} from "./components/pages/PageThree";
+import {Adidas} from "./components/pages/Adidas";
+import {Puma} from "./components/pages/Puma";
+import {Abibas} from "./components/pages/Abibas";
 import {Navigate, NavLink, Route, Routes} from "react-router-dom";
 import {Error404} from "./components/pages/Error404";
+import {Model} from "./components/Model";
 
-
+const PATH = {
+    PAGE1: '/adidas',
+    PAGE2: '/puma',
+    PAGE3: '/abibas',
+    ERROR: "/error404"
+} as const;
 function App() {
-  return (
-      <div>
-        <div className={styles.header}><h1>HEADER</h1></div>
-        <div className={styles.body}>
-          <div className={styles.nav}>
-            <div>
-              <NavLink to='/page1'
-                       className={({isActive}) => isActive ? styles.activeNavLink : styles.navLink}>
-                Page 1
-              </NavLink>
+    return (
+        <div>
+            <div className={styles.header}><h1>HEADER</h1></div>
+            <div className={styles.body}>
+                <div className={styles.nav}>
+                    <div>
+                        <NavLink to={PATH.PAGE1}
+                                 className={({isActive}) => isActive ? styles.activeNavLink : styles.navLink}>
+                            Adidas
+
+                        </NavLink>
+                    </div>
+                    <div>
+                        <NavLink to={PATH.PAGE2}
+                                 className={({isActive}) => isActive ? styles.activeNavLink : styles.navLink}>
+                            Puma
+                        </NavLink>
+                    </div>
+                    <div>
+                        <NavLink to={PATH.PAGE3}
+                                 className={({isActive}) => isActive ? styles.activeNavLink : styles.navLink}>
+                            Abibas
+                        </NavLink>
+                    </div>
+                </div>
+                <div className={styles.content}>
+                    <Routes>
+                        <Route path={PATH.PAGE1} element={<Adidas/>}/>
+                        <Route path={PATH.PAGE2} element={<Puma/>}/>
+                        <Route path={PATH.PAGE3} element={<Abibas/>}/>
+                        <Route path="/" element={<Navigate to={PATH.PAGE1}/>}/>
+                        <Route path={PATH.ERROR} element={<Error404/>}/>
+                        <Route path={'/adidas/:id'} element={<Model/>}/>
+                        <Route path="/*" element={<Navigate to={"/error404"}/>}/>
+                    </Routes>
+                </div>
             </div>
-            <div>
-              <NavLink to='/page2'
-                       className={({isActive}) => isActive ? styles.activeNavLink : styles.navLink}>
-                Page 2
-              </NavLink>
-            </div>
-            <div>
-              <NavLink to='/page3'
-                       className={({isActive}) => isActive ? styles.activeNavLink : styles.navLink}>
-                Page 3
-              </NavLink>
-            </div>
-          </div>
-          <div className={styles.content}>
-            <Routes>
-              <Route path="/page1" element={<PageOne/>}/>
-              <Route path="/page2" element={<PageTwo/>}/>
-              <Route path="/page3" element={<PageThree/>}/>
-              <Route path="/" element={<Navigate to={"/page1"}/>}/>
-              <Route path="/error404" element={<Error404/>}/>
-              <Route path="/*" element={<Navigate to={"/error404"}/>}/>
-            </Routes>
-          </div>
+            <div className={styles.footer}>abibas 2023</div>
         </div>
-        <div className={styles.footer}>abibas 2023</div>
-      </div>
-  );
+    );
 }
 
 
---
+++ src/components/Model.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {adidasArr} from "./pages/Adidas";
+import {Link, useParams} from "react-router-dom";
+
+export const Model = () => {
+    const params = useParams()
+    console.log(params.id)
+    return (
+        <div style={{textAlign: 'center'}}>
+            <h2>{adidasArr[Number(params.id)].model}</h2>
+            <h4>{adidasArr[Number(params.id)].collection}</h4>
+            <h3>{adidasArr[Number(params.id)].price}</h3>
+
+            <img src={adidasArr[Number(params.id)].picture}
+                 alt={adidasArr[Number(params.id)].model}
+                 style={{width: '600px', height: 'auto', marginRight: '10px'}}/>
+        </div>
+    )
+}
